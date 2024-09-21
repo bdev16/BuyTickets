17478c183c000a0385f877cbdc6d3c28b75e880e
DevBruno <brunoantonio.dev0016@gmail.com>
2024-09-21 20:20:42 -0300

    feat(models): adiciona um construtor na classe Flight.cs...
    Este commit adiciona um contrutor na classe Flight. Além de refatorar as
    propiedades presentes na classe, criando atributos privados que serão
    utilizados dentro da mesma...

--- BuyTickets/models/Flight.cs
+++ BuyTickets/models/Flight.cs
@@ -7,12 +7,28 @@ namespace BuyTickets.models
 {
     public class Flight
     {
-        public Guid Id { get; private set; } = Guid.NewGuid();
-        public string Origin { get; private set; }
-        public string Destiny { get; private set; }
-        public DateTime Date { get; private set; }
-        public DateTime DepartureTime { get; private set; }
-        public DateTime ArrivalTime { get; private set; }
+        public Guid Id { get; private set; }
+        private string origin;
+        public string Origin { get { return origin; } set { origin = value; } }
+        private string destiny;
+        public string Destiny { get { return destiny; } set { destiny = value; } }
+        private DateTime date;
+        public DateTime Date { get { return date; } set { date = value; } }
+        private DateTime departureTime;
+        public DateTime DepartureTime { get { return departureTime; } set { departureTime = value; } }
+        private DateTime arrivalTime;
+        public DateTime ArrivalTime  { get { return arrivalTime; } set { arrivalTime = value; } }
         public Enterprise Enterprise{ get; private set; }
+
+        public Flight(string origin, string destiny, DateTime date, DateTime departureTime, DateTime arrivalTime, Enterprise enterprise)
+        {
+            Id = Guid.NewGuid();
+            Origin = origin;
+            Destiny = destiny;
+            Date = date;
+            DepartureTime = departureTime;
+            ArrivalTime = arrivalTime;
+            Enterprise = enterprise;
+        }
     }
 }
