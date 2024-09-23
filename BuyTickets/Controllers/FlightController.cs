52f79abb1f50c80c9cda89d4e3e90fc62b5acb29
DevBruno <brunoantonio.dev0016@gmail.com>
2024-09-23 00:14:58 -0300

    feat(controller): implementa os metodos CRUD da classe FlightController
    É necessário fazer a implementação dos métodos Create, Read, Update e
    delete da classe FlightController, para o sistema ficar funcional...
    
    Este commit implementa os métodos Create, Read, Update e
    delete da classe FlightController,implementação dos métodos Create,
    Read, Update e delete da classe FlightController, construindo a sua
    lógica...

--- BuyTickets/Controllers/FlightController.cs
+++ BuyTickets/Controllers/FlightController.cs
@@ -1,37 +1,100 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using BuyTickets.interfaces;
 using BuyTickets.models;
 
 namespace BuyTickets.Controllers
 {
-    public class FlightController : IController
+    /// <summary>
+    /// This class represents the business logic of the Flight class, containing the methods to Create, Update, List and remove a class from a Flight List
+    /// </summary>
+    public class FlightController : IController<Flight>
     {
-        public void Create()
+        private List<Flight> _flights = new List<Flight>();
+
+        /// <summary>
+        /// Will add flight in the flight list.
+        /// </summary>
+        /// <param name="flight">Will receive a Flight class Object</param>
+        public void Create(Flight flight)
         {
-            throw new NotImplementedException();
+            if (flight == null)
+            {
+                Console.WriteLine("Ocorreu ao tentar criar o voo...");
+            }
+            _flights.Add(flight);
+            Console.WriteLine($"Voo {flight.Id} criado com sucesso!!!");
         }
 
-        public void Delete()
+        /// <summary>
+        /// This method provides access to a copy of the Flight list.
+        /// </summary>
+        /// <returns>Will return the flight list.</returns>
+        public List<Flight> SearchAll()
         {
-            throw new NotImplementedException();
+            return _flights;
         }
 
-        public void SearchAll()
+        /// <summary>
+        /// You will search for a flight in the flight list using your Id attribute.
+        /// </summary>
+        /// <param name="idFlight">Will receive a Guid class which will represent the Flight ID attribute.</param>
+        /// <returns>Will returning the found class</returns>
+        public Flight SearchById(Guid idFlight)
         {
-            throw new NotImplementedException();
+            var resultSearchById = _flights.FirstOrDefault(f => f.Id == idFlight);
+            if (resultSearchById != null)
+            {
+                return resultSearchById;
+            }
+            else
+            {
+                return resultSearchById;
+            }
         }
 
-        public void SearchById()
+        /// <summary>
+        /// It will modify the attributes of a Flight already present in the Flight List.
+        /// </summary>
+        /// <param name="flightUpdate">Will receive a Flight class Object.</param>
+        public void Update(Flight flightUpdate)
         {
-            throw new NotImplementedException();
+
+            var flight = _flights.FirstOrDefault(f => f.Id == flightUpdate.Id);
+            if (flight != null)
+            {
+                flight.Origin = flightUpdate.Origin;
+                flight.Destiny = flightUpdate.Destiny;
+                flight.Date = flightUpdate.Date;
+                flight.DepartureTime = flightUpdate.DepartureTime;
+                flight.ArrivalTime = flightUpdate.ArrivalTime;
+                flight.Enterprise = flightUpdate.Enterprise;
+            }
+            else
+            {
+                Console.WriteLine("O Codigo informado não corresponde a nenhum dos Voos cadastrados no sistema...");
+            }
         }
 
-        public void Update()
+        /// <summary>
+        /// This method will remove the Flight class for the flight list. 
+        /// </summary>
+        /// <param name="idFlight">Will receive a Guid class which will represent the Flight ID attribute.</param>
+        public void Delete(Guid idFlight)
         {
-            throw new NotImplementedException();
+            var resultSearchById = _flights.FirstOrDefault(f => f.Id == idFlight);
+            if (resultSearchById != null)
+            {
+                _flights.Remove(resultSearchById);
+            }
+            else
+            {
+                Console.WriteLine("O Codigo informado não corresponde a nenhum dos Voos cadastrados no sistema...");
+            }
         }
     }
 }
