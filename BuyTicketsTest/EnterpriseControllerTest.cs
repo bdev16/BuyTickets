bb4b9ce8252a9003a36bc30b7f1ccdfede239c95
DevBruno <brunoantonio.dev0016@gmail.com>
2024-09-28 08:42:03 -0300

    test: Cria o método Check_SearchAllMethod_ReturnsAEnterpriseList
    Esse método é necessário para podermos verificar se o Metodo SerachAll da
    classe EnterpriseController estão funcionando de acordo com o que foi
    construido...
    
    Esse commit cria um método na classe EnterpriseControllerTest presente no
    projeto de teste. Esse método vai verificar se as empresas ao tentarem
    ser adicionadas a lista de Empresas presentes na classe EnterpriseController.cs
    estão de fato sendo adicionados nela. O outro objetivo do teste é verificar se
    o metodo searchall está retornando uma copia da lista de empresas
    presentes na classe.

--- BuyTicketsTest/EnterpriseControllerTest.cs
+++ BuyTicketsTest/EnterpriseControllerTest.cs
@@ -28,5 +28,24 @@ namespace BuyTicketsTest
 
             Assert.Equal(enterprise, enterpriseResult);
         }
+
+        [Fact]
+        public void Check_SearchAllMethod_ReturnsAEnterpriseList()
+        {
+            //Arrange
+
+            EnterpriseController enterpriseController = new EnterpriseController();
+            Enterprise enterprise = new Enterprise("LATAM");
+
+            //Act
+
+            enterpriseController.Create(enterprise);
+
+            var enterpriseListResult = enterpriseController.SearchAll();
+
+            //Assert
+
+            Assert.NotEmpty(enterpriseListResult);
+        }
     }
 }
