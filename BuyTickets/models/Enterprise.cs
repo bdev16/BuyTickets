8d24cf853863b08f7d86ce85b9a52015e2d5faee
DevBruno <brunoantonio.dev0016@gmail.com>
2024-09-28 20:26:04 -0300

    feat(models): adiciona novos atributos e propiedades a classe Enterprise.cs
    É necessário que a classe Enterprise tenha novos atributos em seu
    corpo. Esses novos atributos vão estár relacionados com a possibilidade
    de cada empresa fazer login ao sistema e acessar somente os seus
    dados...
    
    Este commit adiciona os atributos e propiedades Email e Password. Esses
    novos atributos vão dar possibilidade de cada empresa conseguir ter
    acesso somente aos propios dados...

--- BuyTickets/models/Enterprise.cs
+++ BuyTickets/models/Enterprise.cs
@@ -10,12 +10,19 @@ namespace BuyTickets.models
         public Guid Id { get; private set; }
         private string name;
         public string Name { get { return name; } set { name = value; } }
+        private string email;
+        public string Email { get { return email;} set { email = value; } }
+        private string password;
+        public string Password { get { return password;} set { password = value; } }
         public List<Flight> Flights { get; private set; }
 
-        public Enterprise(string name)
+        public Enterprise(string name, string email, string password)
         {
             Id = Guid.NewGuid();
-            Name = name;   
+            Name = name;
+            Email = email;
+            Password = password;
+            Flights = new List<Flight>();  
         }
     }
 }
