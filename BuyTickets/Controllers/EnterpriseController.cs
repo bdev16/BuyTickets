97dcd820a84b981c1cf290dd78acb452c7888ff0
DevBruno <brunoantonio.dev0016@gmail.com>
2024-09-28 08:14:06 -0300

    feat(controller): implementa os metodos CRUD da classe EnterpriseController
    É necessário fazer a implementação dos métodos Create, Read, Update e
    delete da classe EnterpriseController, para o sistema ficar funcional...
    
    Este commit implementa os métodos Create, Read, Update e
    delete da classe EnterpriseController,construindo a sua lógica...

--- BuyTickets/Controllers/EnterpriseController.cs
+++ BuyTickets/Controllers/EnterpriseController.cs
@@ -12,29 +12,82 @@ namespace BuyTickets.Controllers
     /// </summary>
     public class EnterpriseController : IController<Enterprise>
     {
-        public void Create(Enterprise value)
+        private List<Enterprise> _enterprises = new List<Enterprise>();
+
+        /// <summary>
+        /// Will add enterprise in the enterprise list.
+        /// </summary>
+        /// <param name="enterprise">Will receive a Enterprise class Object</param>
+        public void Create(Enterprise enterprise)
         {
-            throw new NotImplementedException();
+             if (enterprise == null)
+            {
+                Console.WriteLine("Ocorreu ao tentar cadastrar a Empresa...");
+            }
+
+            _enterprises.Add(enterprise);
+            Console.WriteLine($"Empresa {enterprise.Id} criada com sucesso!!!");
         }
 
-        public void Delete(Guid value)
-        {
-            throw new NotImplementedException();
-        }
-
+        /// <summary>
+        /// This method provides access to a copy of the Enterprise list.
+        /// </summary>
+        /// <returns>Will return the enterprise list.</returns>
         public List<Enterprise> SearchAll()
         {
-            throw new NotImplementedException();
+            return _enterprises;
         }
 
-        public Enterprise SearchById(Guid value)
+        /// <summary>
+        /// You will search for a enterprise in the enterprise list using your Id attribute.
+        /// </summary>
+        /// <param name="idEnterprise">Will receive a Guid class which will represent the Enterprise ID attribute.</param>
+        /// <returns>Will returning the found class</returns>
+        public Enterprise SearchById(Guid idEnterprise)
         {
-            throw new NotImplementedException();
+            var resultSearchById = _enterprises.FirstOrDefault(e => e.Id == idEnterprise);
+            if (resultSearchById != null)
+            {
+                return resultSearchById;
+            }
+            else
+            {
+                return resultSearchById;
+            }
         }
 
-        public void Update(Enterprise value)
+        /// <summary>
+        /// It will modify the attributes of a Enterprise already present in the Enterprise List.
+        /// </summary>
+        /// <param name="enterpriseUpdate">Will receive a Enterprise class Object.</param>
+        public void Update(Enterprise enterpriseUpdate)
         {
-            throw new NotImplementedException();
+            var enterprise = _enterprises.FirstOrDefault(e => e.Id == enterpriseUpdate.Id);
+            if (enterprise != null)
+            {
+                enterprise.Name = enterpriseUpdate.Name;
+            }
+            else
+            {
+                Console.WriteLine("O Codigo informado não corresponde a nenhuma das Empresas cadastradas no sistema...");
+            }
+        }
+
+        /// <summary>
+        /// This method will remove the Enterprise class for the enterprise list. 
+        /// </summary>
+        /// <param name="idEnterprise">Will receive a Guid class which will represent the Enterprise ID attribute.</param>
+        public void Delete(Guid idEnterprise)
+        {
+            var resultSearchById = _enterprises.FirstOrDefault(e => e.Id == idEnterprise);
+            if (resultSearchById != null)
+            {
+                _enterprises.Remove(resultSearchById);
+            }
+            else
+            {
+                Console.WriteLine("O Codigo informado não corresponde a nenhuma das empresas cadastradas no sistema...");
+            }
         }
     }
 }
