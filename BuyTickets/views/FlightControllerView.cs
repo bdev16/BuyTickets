4c7010c177df319b528b3e813cf06bd13fa6333a
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2024-10-08 17:30:05 -0300

    feat(views): Cria a classe FlightControllerView.cs
    Esse commit é necessário para criar a classe FlightControllerView dentro
    da aplicação.
    
    Esse commit cria a classe FlightControllerView dentro da aplicação. A
    mesma vai conter todos os seguintes métodos da classe FlightController:
    Create, SearchById, SearchByEnterprise, Update e Delete. Esses métodos
    vão ser responsaveis por realizar a interação com o usuario, mostrando e
    recebendo dados do mesmo.

---
+++ BuyTickets/views/FlightControllerView.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BuyTickets.Controllers;
+using BuyTickets.models;
+
+
+namespace BuyTickets.views
+{
+    public class FlightControllerView
+    {
+        private FlightController _flightController;
+
+        public FlightControllerView(FlightController flightController)
+        {
+            _flightController = flightController;
+        }
+
+        public void Create(Enterprise enterprise)
+        {
+            Console.WriteLine("Informe o local de origem do voo: ");
+            var origin = Console.ReadLine();
+            Console.WriteLine("Informe o local de destino do voo: ");
+            var destiny = Console.ReadLine();
+            Console.WriteLine("Informe a data do voo: ");
+            var date = Console.ReadLine();
+            Console.WriteLine("Informe a hora de saida do voo: ");
+            var departureTime = Console.ReadLine();
+            Console.WriteLine("Informe a hora de chegada do voo: ");
+            var arrivalTime = Console.ReadLine();
+            Flight flight = new Flight(origin, destiny, date, departureTime, arrivalTime, enterprise);
+            
+            var result = _flightController.Create(flight);
+
+            if (result == null)
+            {
+                Console.WriteLine("O voo não foi criado...");
+            }
+            else
+            {
+                Console.WriteLine($"Voo {flight.Id} criado com sucesso!!!");
+            }
+        }
+
+        public void SearchById()
+        {
+            Console.WriteLine("Informe o ID do voo desejado: ");
+            var flightId = Guid.Parse(Console.ReadLine());
+            var flightResult = _flightController.SearchById(flightId);
+            if (flightResult == null)
+            {
+                Console.WriteLine("Voo não encontrado...");
+            }
+            else
+            {
+                Console.WriteLine($"Codigo Empresa: {flightResult.Enterprise.Id}; Empresa: {flightResult.Enterprise.Name};" + 
+                                    $"\nData: {flightResult.Date}; Saida: {flightResult.DepartureTime}; Chegada: {flightResult.ArrivalTime};" + 
+                                    $"\nCodigo do Voo: {flightResult.Id}");
+            }
+        }
+
+        public void SearchByEnterprise(Enterprise enterprise)
+        {
+            var listEnterpriseFlightsResult = _flightController.SearchByEnterprise(enterprise);
+
+            if (listEnterpriseFlightsResult == null)
+            {
+                Console.WriteLine("Sem voos cadastrados no momento");
+            }
+            else
+            {
+                foreach (var flight in listEnterpriseFlightsResult)
+                {
+                    Console.WriteLine($"Codigo Empresa: {flight.Enterprise.Id}; Empresa: {flight.Enterprise.Name};" + 
+                                    $"\nData: {flight.Date}; Saida: {flight.DepartureTime}; Chegada: {flight.ArrivalTime};" + 
+                                    $"\nCodigo do Voo: {flight.Id}");
+                }
+            }
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Informe o ID do voo desejado: ");
+            var flightId = Guid.Parse(Console.ReadLine());
+            var flightResult = _flightController.SearchById(flightId);
+            if (flightResult == null)
+            {
+                Console.WriteLine("Voo não encontrado...");
+            }
+            else
+            {
+                Console.WriteLine("Os dados informados vão modificar os dados do voo existente.");
+                Console.WriteLine("Informe o local de origem do voo: ");
+                flightResult.Origin = Console.ReadLine();
+                Console.WriteLine("Informe o local de destino do voo: ");
+                flightResult.Destiny = Console.ReadLine();
+                Console.WriteLine("Informe a data do voo: ");
+                flightResult.Date = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Informe a hora de saida do voo: ");
+                flightResult.DepartureTime = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Informe a hora de chegada do voo: ");
+                flightResult.ArrivalTime = DateTime.Parse(Console.ReadLine());
+                var resultUpdateFlight = _flightController.Update(flightResult);
+                
+                if (resultUpdateFlight == null)
+                {
+                    Console.WriteLine("Ocorreu um erro ao realizar as modificações");
+                }
+                else
+                {
+                    Console.WriteLine($"Voo {resultUpdateFlight.Id} foi modificado com sucesso!!!");
+                }
+            }
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Informe o ID do voo desejado: ");
+            var flightId = Guid.Parse(Console.ReadLine());
+            var flightResult = _flightController.SearchById(flightId);
+            if (flightResult == null)
+            {
+                Console.WriteLine("Voo não encontrado...");
+            }
+            else
+            {
+                var resultDeleteFlight = _flightController.Delete(flightResult.Id);
+
+                if (resultDeleteFlight == null)
+                {
+                    Console.WriteLine($"Ocorreu um erro ao tentar deletar o voo informado...");
+                }
+                else
+                {
+                    Console.WriteLine($"Voo {flightResult.Id} deletado com sucesso!!!");
+                }
+            }
+        }
+    }
+}
