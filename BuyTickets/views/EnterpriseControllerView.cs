45d89e9fff27181a2ed5329747df51efa470099f
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2024-10-08 17:35:39 -0300

    feat(views): Cria a classe EnterpriseControllerView.cs
    Esse commit é necessário para criar a classe EnterpriseControllerView.cs
    dentro da aplicação.
    
    Esse commit cria a classe EnterpriseControllerView.cs dentro da
    aplicação. A mesma vai conter os seguintes métodos da classe
    EnterpriseController: Create, SearchById, Update, Login. Esses métodos
    vão ser responsaveis por fazer a interação com o usuario, recebendo
    dados e informandos dados a ele.

---
+++ BuyTickets/views/EnterpriseControllerView.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BuyTickets.Controllers;
+using BuyTickets.models;
+
+namespace BuyTickets.views
+{
+    public class EnterpriseControllerView
+    {
+        private EnterpriseController _enterpriseController;
+        private MenuView _menuView;
+
+        public EnterpriseControllerView(EnterpriseController enterpriseController, MenuView menuView)
+        {
+            
+            _enterpriseController = enterpriseController;
+            _menuView = menuView;
+        }
+
+        public void Create()
+        {
+            Console.WriteLine("Informe o nome da empresa: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Informe o email: ");
+            var email = Console.ReadLine();
+            Console.WriteLine("Informe a senha");
+            var password = Console.ReadLine();
+            Enterprise enterprise = new Enterprise(name, email, password);
+
+            var result = _enterpriseController.Create(enterprise);
+
+            if (result == null)
+            {
+                Console.WriteLine("Ocorreu um erro no cadastro da empresa...");
+            }
+            else
+            {
+                Console.WriteLine($"Empresa {result.Id} cadastrada com sucesso!!!");
+            }
+        }
+
+        public void SearchById(Enterprise enterprise)
+        {
+            var enterpriseResult = _enterpriseController.SearchById(enterprise.Id);
+
+            if (enterpriseResult == null)
+            {
+                Console.WriteLine("A empresa informada não foi encontrada...");
+            }
+            else
+            {
+                Console.WriteLine($"Nome: {enterpriseResult.Name};\nEmail: {enterpriseResult.Email};\nSenha: {enterpriseResult.Password}");
+            }
+        }
+
+        public void Update(Enterprise enterprise)
+        {
+            var enterpriseResult = _enterpriseController.SearchById(enterprise.Id);
+            if (enterpriseResult == null)
+            {
+                Console.WriteLine("A empresa informada não foi encontrada...");
+            }
+            else
+            {
+                enterpriseResult.Name = Console.ReadLine();
+                enterpriseResult.Email = Console.ReadLine();
+                enterpriseResult.Password = Console.ReadLine();
+                var enterpriseUpdateResult = _enterpriseController.Update(enterpriseResult);
+
+                if (enterpriseUpdateResult == null)
+                {
+                    Console.WriteLine("Ocorreu um erro ao tentar modificar os dados da empresa...");
+                }
+                else
+                {
+                    Console.WriteLine($"Empresa {enterpriseResult.Id} foi modificada com sucesso!!!");
+                }
+            }     
+        }
+
+        public void Login()
+        {
+            Console.WriteLine("Informe o email: ");
+            var email = Console.ReadLine();
+            Console.WriteLine("Informe a senha: ");
+            var password = Console.ReadLine();
+            var loginResult = _enterpriseController.Login(email, password);
+
+            if (loginResult == null)
+            {
+                Console.WriteLine("Email ou senha invalidos...");
+            }
+            else
+            {
+                _menuView.EnterpriseMenu(loginResult);
+            }
+        }
+    }
+}
