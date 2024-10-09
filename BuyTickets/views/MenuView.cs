2c351a49606481eb58b296800ccbae6b8f7ccda7
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2024-10-09 14:58:08 -0300

    feat(views): Cria o método MainMenu dentro da classe MenuView.cs
    Esse método vai servir como um menu principal para o usuario que vai
    utilizar a aplicação, ao iniciar a aplicação o usuario vai ser jogado
    nesse método e ao sair de outros métodos com o mesmo intuito desse ele
    vai ser retornado nele novamente.
    
    Esse commit cria o método MainMenu dentro da classe MenuView.cs, que vai
    conter as ações que o usuario vai poder realizar dentro da aplicação,
    esse método vai se ligar com as outras partes do sistema dando a
    possibilidade do usuario utilizar todas as funcionalidades dele.

--- BuyTickets/views/MenuView.cs
+++ BuyTickets/views/MenuView.cs
@@ -18,5 +18,48 @@ namespace BuyTickets.views
             _enterpriseControllerView = enterpriseControllerView;
         }
 
+        public void MainMenu(MenuView menuView)
+        {
+            bool exitSystem = false;
+
+            while (exitSystem != true)
+            {
+                Console.Clear();
+                Console.WriteLine("=================================");
+                Console.WriteLine("            BuyFlights           ");
+                Console.WriteLine("=================================");
+                Console.WriteLine();
+                Console.WriteLine("[1]Empresas.\n[0]Sair do sistema.");
+                var optionUser = Convert.ToInt32(Console.ReadLine());
+                switch (optionUser)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("[1]Login.\n[2]Cadastro.");
+                        var optionUser2 = Convert.ToInt32(Console.ReadLine());
+                        switch(optionUser2)
+                        {
+                            case 1:
+                                _enterpriseControllerView.Login(menuView);
+                                break;
+                            case 2:
+                                _enterpriseControllerView.Create();
+                                break;
+                        }
+                        Console.ReadKey();
+                        break;
+                    case 0:
+                        Console.Clear();
+                        Console.WriteLine("Saindo do sistema...");
+                        exitSystem = true;
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine($"A opção [{optionUser}] não está presente na lista de opções disponiveis...");
+                        break;
+                }                 
+            }
+        }
+   
     }
 }
