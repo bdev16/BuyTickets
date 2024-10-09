80056ef9d2e6553a1c55087ca3a078041c557a03
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2024-10-09 15:02:26 -0300

    feat(views): Cria o método EnterpriseMenu dentro da classe MenuView.cs
    Esse método vai servir como um menu secundario voltado para o usuario empresa
    que vai ser um dos dois tipos de usuario que vai utilizar o sistema.
    
    Esse commit cria o método EnterpriseMenu dentro da classe MenuView.cs, que vai
    conter as ações que o usuario empresa vai poder realizar dentro da aplicação,
    esse método vai se ligar com as outras partes do sistema dando a
    possibilidade do usuario empresa utilizar todas as funcionalidades
    aderidas a ele.

--- BuyTickets/views/MenuView.cs
+++ BuyTickets/views/MenuView.cs
@@ -61,5 +61,78 @@ namespace BuyTickets.views
             }
         }
    
+        public void EnterpriseMenu(Enterprise enterprise)
+        {
+            bool exitAccount = false;
+            bool backToMenu = false;
+
+            while (exitAccount != true)
+            {
+                Console.Clear();
+                Console.WriteLine("=================================");
+                Console.WriteLine("            BuyFlights           ");
+                Console.WriteLine("=================================");
+                Console.WriteLine();
+                Console.WriteLine("[1]Ver voos cadastrados.\n[2]Cadastrar voo.\n[3]Editar voo.\n[4]Excluir voo.\n[5]Conta.\n[0]Sair da conta.");
+                var optionUser = Convert.ToInt32(Console.ReadLine());
+                switch (optionUser)
+                {
+                    case 1:
+                        Console.Clear();
+                        _flightControllerView.SearchByEnterprise(enterprise);
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        _flightControllerView.Create(enterprise);
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        _flightControllerView.Update();
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        _flightControllerView.Delete();
+                        Console.ReadKey();
+                        break;
+                    case 5:
+                        while (backToMenu != false)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("[1]Dados.\n[2]Editar dados.\n[0]Voltar");
+                            var optionUser2 = Convert.ToInt32(Console.ReadLine());
+                            switch (optionUser2)
+                            {
+                                case 1:
+                                    Console.Clear();
+                                    _enterpriseControllerView.SearchById(enterprise);
+                                    Console.ReadKey();
+                                    break;
+                                case 2:
+                                    Console.Clear();
+                                    _enterpriseControllerView.Update(enterprise);
+                                    Console.ReadKey();
+                                    break;
+                                case 0:
+                                    backToMenu = true;
+                                    break;
+                            }
+                        }
+                        break;
+                    case 0:
+                        Console.Clear();
+                        Console.WriteLine("Voltando para o menu principal...");
+                        exitAccount = true;
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine($"A opção [{optionUser}] não está presente na lista de opções disponiveis...");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
     }
 }
