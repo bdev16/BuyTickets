6a3fd84def59e2821535a3a13b974c096ec2feee
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2024-10-09 14:52:28 -0300

    feat(views): Adiciona o mecanismo de injeção de depêndencia na classe MenuView
    O mecanismo de injeção de dependência dentro dessa classe vai servir
    para podermos utilizar as mesmas instâncias das classes
    FlightControllerView e EnterpriseControllerView sem precisar cria-las
    dentro da classe atual e sem precisar chama-las via parâmetro dos
    métodos.
    
    O commit adiciona o mecanismo de injeção de depêndencia na classe
    MenuView, recebendo as instâncias das classes FlightControllerView e
    EnterpriseControllerView.

--- BuyTickets/views/MenuView.cs
+++ BuyTickets/views/MenuView.cs
@@ -2,11 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BuyTickets.models;
+
 
 namespace BuyTickets.views
 {
     public class MenuView
     {
-        
+        private readonly FlightControllerView _flightControllerView;
+        private readonly EnterpriseControllerView _enterpriseControllerView;
+
+        public MenuView(FlightControllerView flightControllerView, EnterpriseControllerView enterpriseControllerView)
+        {
+            _flightControllerView = flightControllerView;
+            _enterpriseControllerView = enterpriseControllerView;
+        }
+
     }
 }
