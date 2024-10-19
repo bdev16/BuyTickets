d3c504bc20b2792f22894e455858879d1d7e5c5f
DevBruno <brunoantonio.dev0016@gmail.com>
2024-10-19 19:48:44 -0300

    fix: soluciona o bug referente ao lançamento das notificações dentro de GlobalValidations
    Esse commit tem o intuito de solucionar um bug existente dentro da
    classe GlobalValidations e acabava impactando a lógica de negocios do
    sistema.
    
    Esse commit coluciona o bug referente ao lançamento das notificações dentro
    de GlobalValidations. As notificações não eram lançadas da maneira
    correta, permitindo que os valores errados informados pelo usuario
    conseguissem chegar na criação do objeto de flight, gerando execeções de
    formato de tipo. Foi trocada o método IsNotNull por IsNotNullOrEmpty, já
    que os valores não chegavam com valores do tipo null, mas sim como
    valores vazios do tipo string "", o que permitia a passagem do codigo
    para outras areas do sistema. Além disso foi alterado a forma de tentar
    converter os valores informados para o tipo DateTime, antes era
    utilizado o método TryParse utilizando uma unica instância do tipo
    datetime, agora utilizamos o métodoTryParseExact e uma instância para
    cada variavel que seria convertida para o tipo DateTime.

--- BuyTickets/GlobalValidations.cs
+++ BuyTickets/GlobalValidations.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Flunt.Notifications;
@@ -13,21 +14,31 @@ namespace BuyTickets
         {
             //O metodo Clear limpa todas as notificações que foram geradas e adicionadas em Notifiable
             Clear();
-            //Cria uma variavel do tipo DateTime que vai servir como uma referencia
-            DateTime dateFormat;
+            //Cria uma variavel do tipo DateTime que vai servir como uma referencia para verificar se os dados informados pelos
+            //usuarios vão estar de acordo com o tipo DateTime
+            DateTime dateFormat, departureTimeFormat, arrivalTimeFormat;
+            //Vai permitir que a conversão de Cultura de lingua seja feita através de valores informados dentro do codigo, de maneira automatica
+            CultureInfo cultureInfo = CultureInfo.InvariantCulture;
+
+            var departureTimeWithDateInclude = $"{date} {departureTime}";
+            var arrivalTimeWithDateInclude = $"{date} {arrivalTime}";
+            
             //Os codigos abaixo tentam fazer a conversão dos valores que vão ser utilizados para o tipo datetime
-            bool isValidDate = DateTime.TryParse(date, out dateFormat);
-            bool isValidDepartureTime = DateTime.TryParse(departureTime, out dateFormat);
-            bool isValidArrivalTime = DateTime.TryParse(arrivalTime, out dateFormat);
+            //O tryparseexact da classe datetime permite tentar realizar a conversão de maneira mais detalhada e evitando o lançamento de exceções
+            //Se a conversão for possivel as variaveis receberam o valor FALSE, se forem possiveis de serem feitas retornaram valores TRUE
+            bool isValidDate = DateTime.TryParseExact(date,"dd/MM/yyyy", cultureInfo, DateTimeStyles.None, out dateFormat);
+            bool isValidDepartureTime = DateTime.TryParseExact(departureTimeWithDateInclude, "dd/MM/yyyy HH:mm", cultureInfo, DateTimeStyles.None, out departureTimeFormat);
+            bool isValidArrivalTime = DateTime.TryParseExact(arrivalTimeWithDateInclude, "dd/MM/yyyy HH:mm", cultureInfo, DateTimeStyles.None, out arrivalTimeFormat);
+            // var contract = new Contract<Notification>().
             var contract = new Contract<Notification>().
                 Requires().
-                IsNotNull(origin, "Origem", "O local de origem nao pode ser vazio").
-                IsNotNull(destiny, "Destino", "O local de destino nao pode ser vazio").
-                IsNotNull(date, "Data", "A data do voo nao pode ser vazia").
+                IsNotNullOrEmpty(origin, "Origem", "O local de origem nao pode ser vazio").
+                IsNotNullOrEmpty(destiny, "Destino", "O local de destino nao pode ser vazio").
+                IsNotNullOrEmpty(date, "Data", "A data do voo nao pode ser vazia").
                 IsTrue(isValidDate, "Data", "A data informada nao e uma data valida").
-                IsNotNull(departureTime, "Horario de saida", "O horario de saida nao pode ser vazio").
+                IsNotNullOrEmpty(departureTime, "Horario de saida", "O horario de saida nao pode ser vazio").
                 IsTrue(isValidDepartureTime, "Horario de saida", "O horario de saida informado nao segue a estrutura de um horario valida").
-                IsNotNull(arrivalTime, "Horario de chegada", "O horario de chegada nao pode ser vazio").
+                IsNotNullOrEmpty(arrivalTime, "Horario de chegada", "O horario de chegada nao pode ser vazio").
                 IsTrue(isValidArrivalTime, "Horario de chegada", "O horario de chegada informado nao segue a estrutura de um horario valida");
             
             AddNotifications(contract);
@@ -52,10 +63,10 @@ namespace BuyTickets
             Clear();
             var contract = new Contract<Notification>().
                 Requires().
-                IsNotNull(name, "Nome", "O nome nao pode ser vazio").
-                IsNotNull(email, "Email", "O email nao pode ser vazio").
+                IsNotNullOrEmpty(name, "Nome", "O nome nao pode ser vazio").
+                IsNotNullOrEmpty(email, "Email", "O email nao pode ser vazio").
                 IsEmail(email, "Email", "O email informado nao e valido").
-                IsNotNull(password, "Senha", "A senha nao pode ser vazia");
+                IsNotNullOrEmpty(password, "Senha", "A senha nao pode ser vazia");
                 
             AddNotifications(contract.Notifications);
             // var copyListNotifications = contract.Notifications;
