47dc577614209c389a7b159ee7069b2c495bcde9
DevBruno <brunoantonio.dev0016@gmail.com>
2024-10-20 17:12:53 -0300

    feat(models): cria a classe User.cs dentro do sistema
    Esse commit tem o intuito de criar uma classe base para todas as classes
    que sejam uma abstração de um tipo de usuario do sistema.
    
    A classe user vai servir como uma classe base, contendo os atributos
    genericos para as classes do tipo usuario, reutilizando-os.

---
+++ BuyTickets/models/User.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuyTickets.models
+{
+    public abstract class User
+    {
+        public Guid Id { get; private set; }
+        private string fullName;
+        public string FullName { get { return fullName; } set { fullName = value; } }
+        private string email;
+        public string Email { get { return email;} set { email = value; } }
+        private string password;
+        public string Password { get { return password;} set { password = value; } }
+
+        public User (string fullName, string email, string password)
+        {
+            Id = Guid.NewGuid();
+            FullName = fullName;
+            Email = email;
+            Password = password;
+        }
+    }
+}
