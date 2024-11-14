27f5b9a35f7bfa8ecd2646cc053e095a45170656
DevBruno <brunoantonio.dev0016@gmail.com>
2024-11-14 10:34:31 -0300

    feat: cria uma lista de Airports

--- BuyTickets/Program.cs
+++ BuyTickets/Program.cs
@@ -19,7 +19,33 @@ List<Flight> flightList = new List<Flight>
     new Flight("Manaus", "Belém", "2024-11-14", "12:00", "14:00", enterprise),
 };
 
-flights = flightList;
+List<Airport> airports = new List<Airport>
+{
+    new Airport("RIO BRANCO", "AC"), new Airport("MACAPA", "AP"),
+    new Airport("MANAUS", "AM"), new Airport("BELEM", "PA"),
+    new Airport("PORTO VELHO", "RO"), new Airport("BOA VISTA", "RR"),
+    new Airport("PALMAS", "TO"), new Airport("MACEIO", "AL"),
+    new Airport("SALVADOR", "BA"), new Airport("FORTALEZA", "CE"),
+    new Airport("SAO LUIZ",  "(MA)"), new Airport("JOAO PESSOA", "PB"),
+    new Airport("RECIFE", "PE"), new Airport("TERESINA", "PI"),
+    new Airport("NATAL", "RN"), new Airport("ARACAJU", "SE"),
+    new Airport("BRASILIA", "DF"), new Airport("GOIANIA", "GO"),
+    new Airport("CUIABA", "MT"), new Airport("CAMPO GRANDE", "MS"),
+    new Airport("VITORIA", "ES"), new Airport("BELO HORIZONTE", "MG"),
+    new Airport("RIO DE JANEIRO", "RJ"), new Airport("SAO PAULO", "SP"),
+    new Airport("CURITIBA", "PR"), new Airport("PORTO ALEGRE", "RS"),
+    new Airport("FLORIANOPOLIS", "SC"), new Airport("JOAO PESSOA", "PB"),
+    new Airport("RECIFE", "PE"), new Airport("TERESINA", "PI"),
+    new Airport("NATAL", "RN"), new Airport("ARACAJU", "SE"),
+    new Airport("BRASILIA", "DF"), new Airport("GOIANIA", "GO"),
+    new Airport("CUIABA", "MT"), new Airport("CAMPO GRANDE", "MS"),
+    new Airport("VITORIA", "ES"), new Airport("BELO HORIZONTE", "MG"),
+    new Airport("RIO DE JANEIRO", "RJ"), new Airport("SAO PAULO", "SP"),
+    new Airport("CURITIBA", "PR"), new Airport("PORTO ALEGRE", "RS"),
+    new Airport("FLORIANOPOLIS", "SC"),
+};
+
+var resultValueInAirports = airports.FirstOrDefault(a => a.City == "CURITIBA");
 
 Customer customer = new Customer("emailvalido@gmail.com", "cliente123", "Bruno", "Antonio", "56097045080");
 customers.Add(customer);
@@ -28,7 +54,7 @@ GlobalValidations globalValidations = new GlobalValidations();
 EnterpriseController enterpriseController = new EnterpriseController(enterprises);
 EnterpriseControllerView enterpriseControllerView = new EnterpriseControllerView(enterpriseController, globalValidations);
 FlightController flightController = new FlightController(flights);
-FlightControllerView flightControllerView = new FlightControllerView(flightController, globalValidations);
+FlightControllerView flightControllerView = new FlightControllerView(flightController, globalValidations, airports);
 CustomerController customerController= new CustomerController(customers);
 CustomerControllerView customerControllerView = new CustomerControllerView(customerController,flightController, globalValidations);
 
