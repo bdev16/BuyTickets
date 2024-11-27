5e4137b735bca0f3b8d54eaf0499abbba80a753b
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2024-11-27 16:26:35 -0300

    feat: Cria o metodo Create_ShouldCreateFlight_WhenTheInputsIsValid na classe FlightControllerViewTest
    Esse commit tem o intuito de testar o método Create da classe
    FlightControllerView, verificando se ele consegue cadastrar um voo da
    maneira correta seguindo o cenário bom de criação de voo

--- BuyTicketsTest/FlightControllerViewTest.cs
+++ BuyTicketsTest/FlightControllerViewTest.cs
@@ -24,5 +24,53 @@ namespace BuyTicketsTest
             _output = output;
             _fixture = fixture;
         }
+
+         [Fact]
+        public void Create_ShouldCreateFlight_WhenTheInputsIsValid()
+        {
+            // Arrange
+            var enterprise = _fixture.Enterprise;
+            var flightControllerView = _fixture.FlightControllerView;
+            var flightController = _fixture.FlightController;
+
+            //
+            using (var input = new StringReader("manaus\nbelem\n30/12/2024\n08:00\n10:00"))
+            using (var output = new StringWriter())
+            {
+                //Muda o padrão de entrada de dados
+                Console.SetIn(input);
+                //Muda o padrão de saida de dados
+                Console.SetOut(output);
+
+                // Act
+                flightControllerView.Create(enterprise);
+
+                // Assert
+                var consoleOutputResult = output.ToString();
+
+                // Dividindo saída em linhas
+                var linesToConsoleOutput = consoleOutputResult.Split(Environment.NewLine);
+
+                // Obtendo a 6ª linha onde está o ID do voo
+                var linesForIdFlight = linesToConsoleOutput[5];
+
+                // Extraindo o GUID
+                var idFlight = linesForIdFlight.Substring(4, 36); // GUID tem 36 caracteres no formato padrão.
+
+                // Buscando o voo pelo ID
+                var resultCreateFlight = flightController.SearchById(Guid.Parse(idFlight));
+
+                //Excluindo o Voo criado
+                flightController.Delete(Guid.Parse(idFlight));
+
+                //Verificando se o voo foi excluido
+                var resultDeleteFlight = flightController.SearchById(Guid.Parse(idFlight));
+
+                // Validando que o voo foi criado corretamente
+                Assert.NotNull(resultCreateFlight);
+                Assert.Equal(null, resultDeleteFlight);
+            }
+        }
+
     }
 }
