b1647173f96089aa85dc4fccd061f69a26b35492
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2024-11-27 16:21:33 -0300

    feat: Cria a classe FlightControllerViewTest no projeto de teste
    Esse commit tem o intuito de ter uma classe que vai gerir todos os
    métodos de testes relacionados as views de flightController

---
+++ BuyTicketsTest/FlightControllerViewTest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using BuyTickets.models;
+using BuyTickets.Controllers;
+using System.ComponentModel.Design;
+using BuyTickets.views;
+using BuyTickets;
+using BuyTicketsTest;
+using Xunit.Abstractions;
+using System.Text;
+
+namespace BuyTicketsTest
+{
+    public class FlightControllerViewTest
+    {
+        
+        
+    }
+}
