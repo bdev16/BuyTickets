f500e92d45116503ab1534ee93c83129fd11756d
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2024-11-27 16:36:40 -0300

    feat: Cria o metodo SearchById_ShouldReturnFlight_InformedByParameter_ShowingThePassengers da classe FlightControllerViewTest
    Esse commit tem o intuito de testar se o método SearchById da classe
    FlightControllerView. No cenário em que o usuario empresa quer ver
    os passageiros do seu voo, porém ainda não tem nenhum passageiro dentro
    do voo pesquisado.

--- BuyTicketsTest/FlightControllerViewTest.cs
+++ BuyTicketsTest/FlightControllerViewTest.cs
@@ -171,5 +171,79 @@ namespace BuyTicketsTest
             // Assert.False(true, "Forçando falha para ver saída.");
         }
 
+        [Fact]
+        public void SearchById_ShouldReturnFlight_InformedByParameter_ShowingThePassengers()
+        {
+            // Arrange
+
+            var enterprise = _fixture.Enterprise;
+            var flightControllerView = _fixture.FlightControllerView;
+            var customerOrEnterprise = new Tuple<Customer, Enterprise>(null, enterprise);
+            string idFlight;
+            
+            // Act
+
+            flightControllerView.SearchAll();
+
+            // _output.WriteLine($"Número de voos na lista: {_fixture.Flights.Count}");
+            // Assert.False(true, "Forçando falha para ver saída.");
+
+            using (var output = new StringWriter())
+            {
+                // Muda o padrão de saida de dados
+                Console.SetOut(output);
+
+                flightControllerView.SearchAll();
+                
+                var outputResult = output.ToString();
+                // _output.WriteLine($"{outputResult}");
+
+                var linesOutputResult = outputResult.Split('\n');
+
+                var lineToIdFlight = linesOutputResult[2];
+
+                idFlight = lineToIdFlight.Substring(15, 36);
+                // output.WriteLine($"{idFlight}");
+
+                Assert.Equal($"Codigo Empresa: {enterprise.Id}; Empresa: {enterprise.FullName};" +
+                                        $"Origem: RIO BRANCO (AC); Destino: MACAPA (AP)" +
+                                        $"\nData: 30/12/2024 00:00:00; Saida: 30/12/2024 08:00:00; Chegada: 30/12/2024 10:00:00;" + 
+                                        $"\nCodigo do Voo: {idFlight}\n{Environment.NewLine}", outputResult);
+            }
+
+            using (var input = new StringReader($"{idFlight}\n1"))
+            using (var output = new StringWriter())
+            {
+                //Muda o padrão de entrada de dados
+                Console.SetIn(input);
+                //Muda o padrão de saida de dados
+                Console.SetOut(output);
+
+                flightControllerView.SearchById(customerOrEnterprise);
+
+                var consoleOutputResult = output.ToString();
+
+                // Dividindo saída em linhas
+                var linesToConsoleOutput = consoleOutputResult.Split(Environment.NewLine);
+
+                // Obtendo a 6ª linha onde está o ID do voo
+                var dataToFlightInformed = linesToConsoleOutput[1];
+
+                //Obtendo a 4 linha do output onde está o resultado da busca por passageiros no voo
+                var dataToPassagersAtTheFlight = linesToConsoleOutput[3];
+                _output.WriteLine($"{dataToPassagersAtTheFlight}");
+
+                //Assert
+
+                // Validando que o voo foi criado corretamente
+                Assert.Equal($"Codigo Empresa: {enterprise.Id}; Empresa: {enterprise.FullName};" +
+                                        $"Origem: RIO BRANCO (AC); Destino: MACAPA (AP)" +
+                                        $"\nData: 30/12/2024 00:00:00; Saida: 30/12/2024 08:00:00; Chegada: 30/12/2024 10:00:00;" + 
+                                        $"\nCodigo do Voo: {idFlight}\n", dataToFlightInformed);
+
+                Assert.Equal("Não existem clientes cadastrados a esse voo...", dataToPassagersAtTheFlight);
+            }
+        }
+
     }
 }
