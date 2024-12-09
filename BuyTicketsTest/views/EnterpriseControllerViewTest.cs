816d0dbcd0083ddf60089f7ee4fe5fce9cc1cf10
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2024-12-09 19:00:31 -0300

    feat: Cria o método Create_ShouldCreateEnterprise_WhenTheInputsIsValid dentro da classe EnterpriseControllerViewTest

--- BuyTicketsTest/views/EnterpriseControllerViewTest.cs
+++ BuyTicketsTest/views/EnterpriseControllerViewTest.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BuyTickets.Controllers;
 using BuyTickets.models;
+using BuyTickets.views;
 using Xunit.Abstractions;
 
 namespace BuyTicketsTest.views
@@ -16,6 +18,57 @@ namespace BuyTicketsTest.views
         {
             _output = output;
             _fixture = fixture;
+        }
+
+        [Fact]  
+        public void Create_ShouldCreateEnterprise_WhenTheInputsIsValid()
+        {
+            // Arrange
+            
+            EnterpriseController enterpriseController = _fixture.EnterpriseController;
+            EnterpriseControllerView enterpriseControllerView = _fixture.EnterpriseControllerView;
+
+            //Act
+
+            using (var input = new StringReader("gol\ngol.airlines@gmail.com\ngol123\n35062925638499"))
+            using (var output = new StringWriter())
+            {
+                //Muda o padrão de entrada de dados
+                Console.SetIn(input);
+                //Muda o padrão de saida de dados
+                Console.SetOut(output);
+                
+                enterpriseControllerView.Create();
+
+                // Assert
+                var consoleOutputResult = output.ToString();
+
+                // Dividindo saída em linhas
+                var linesToConsoleOutput = consoleOutputResult.Split(Environment.NewLine);
+
+                // Obtendo a 6ª linha onde está o ID do voo
+                var linesForIdEnterprise = linesToConsoleOutput[4];
+                _output.WriteLine($"{linesForIdEnterprise}");
+
+                // Extraindo o GUID
+                var idEnterprise = linesForIdEnterprise.Substring(8, 36);
+                _output.WriteLine($"{idEnterprise}"); // GUID tem 36 caracteres no formato padrão.
+
+                // Buscando o voo pelo ID
+                var resultCreateEnterprise = enterpriseController.SearchById(Guid.Parse(idEnterprise));
+
+                // //Excluindo o Voo criado
+                // EnterpriseController.Delete(Guid.Parse(idEnterprise));
+
+                // //Verificando se o voo foi excluido
+                // var resultDeleteFlight = flightController.SearchById(Guid.Parse(idFlight));
+
+                // // Validando que o voo foi criado corretamente
+                // Assert.NotNull(resultCreateFlight);
+                Assert.Equal(2, 5);
+                // Assert.Equal(null, resultDeleteFlight);
+
+            }
         }
     }
 }
