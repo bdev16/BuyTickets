0122513464cbb04c0329f13925d063fed8da1fcb
BRUNO ANTONIO ALEXANDRINO DA SILVA <bruno.alexandrino@a.ficr.edu.br>
2024-12-11 16:16:10 -0300

    feat: Cria a classe de testes FlightControllerViewTest e Adiciona as instâncias de FixtureControllersAndViews

---
+++ BuyTicketsTest/views/CustomerControllerViewTest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BuyTickets.models;
+using Xunit.Abstractions;
+
+namespace BuyTicketsTest.views
+{
+    public class CustomerControllerViewTest : IClassFixture<FixtureControllersAndViews>
+    {
+        
+        private readonly FixtureControllersAndViews _fixture;
+        private readonly ITestOutputHelper _output;
+
+        public CustomerControllerViewTest(FixtureControllersAndViews fixture, ITestOutputHelper output)
+        {
+            _output = output;
+            _fixture = fixture;
+        }
+    }
+}
