bc1e7e7c1552fd1822e945df94dfd6cf6a80d64e
DevBruno <brunoantonio.dev0016@gmail.com>
2024-12-26 14:37:19 -0300

    feat: adiciona o EF ao projeto

--- BuyTickets/obj/Debug/net8.0/BuyTickets.AssemblyInfo.cs
+++ BuyTickets/obj/Debug/net8.0/BuyTickets.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuyTickets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc69dd509fd3c85e8383335a84c1e634d7ab294")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e404c66965b2185cc41effb185d35804be78ae11")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuyTickets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuyTickets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
